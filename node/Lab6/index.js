a14e79db9d60ffe7e7ec513bdf9be825a1d2cc6e
UndecidedTech <bpowers.soccer@gmail.com>
2020-02-19 21:27:41 -0500

    update

--- node/Lab6/index.js
+++ node/Lab6/index.js
@@ -4,11 +4,6 @@ const ip = require("ip");
 const app = express();
 const port = 3000;
 
-
-console.log(os.uptime())
-
-
-
 function returnServerInfo() {
     let uptime = os.uptime()
 
@@ -41,19 +36,47 @@ function returnServerInfo() {
 
     return `
         <html>
-        <p>Hostname: ${data.hostname}</p>
-        <p>Ip Address: ${data.IpAddress}</p>
-        <p>Number of Cpus: ${data.Cpus}</p>
-        <p>Uptime: ${JSON.stringify(data.Uptime)}</p>
-        <p>Total memory: ${data.TotalMem}</p>
-        <p>Free memory: ${data.FreeMem}</p>
+        <head>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <link rel="stylesheet" type="text/css" href="theme.css">
+        </head>
+        <div class="container mt-5">
+            <div class="row pl-4 pt-3">
+                <div class="col">Hostname: ${data.Hostname}</div>
+            </div>
+            <hr/>
+            <div class="row pl-4 pt-3">
+                <div class="col data">Ip Address: ${data.IpAddress}</div>
+            </div>
+            <hr/>
+            <div class="row pl-4 pt-3">
+                <div class="col">Number of Cpus: ${data.Cpus}</div>
+            </div>
+            <hr/>
+            <div class="row pl-4 pt-3">
+                <div class="col">Uptime: ${JSON.stringify(data.Uptime)}</div>
+            </div>
+            <hr/>
+            <div class="row pl-4 pt-3">
+                <div class="col">Total memory: ${data.TotalMem}</div>
+            </div>
+            <hr/>
+            <div class="row pl-4 pt-3 pb-3">
+                <div class="col">Free memory: ${data.FreeMem}</div>
+            </div>
+            <button class="btn btn-info" onclick="location.reload()">Refresh Data</button>
+        </div>
         </html>
     `
 }
 
 app.listen(port, () => console.log(`server started on ${port}`));
-
+//serve data and HTML
 app.get("/", (req, res) => {
     res.send(returnServerInfo());
 })
 
+//serve CSS
+app.get("/theme.css", (req, res) => {
+    res.sendFile(__dirname + "/" + "theme.css")
+})
