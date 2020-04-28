cc1bf555467d3396a48e34a563eb1857a69d3978
UndecidedTech <bpowers.soccer@gmail.com>
2020-04-28 17:34:45 -0400

    update

--- Labs/Lab9/test.py
+++ Labs/Lab9/test.py
@@ -4,6 +4,11 @@ import requests
 r = requests.get('http://localhost:3000')
 
 data = r.json()
-
+stringArray = []
 for p in data:
+    sentence = "{0} is color: {1}".format(p["name"], p["color"])
+    stringArray.append(sentence)
     print("{0} is color: {1}".format(p["name"], p["color"]))
+
+
+print(stringArray)
